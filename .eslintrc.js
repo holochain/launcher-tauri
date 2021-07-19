0579750fd0269ac4fd7b7db838d2d37467ccd273
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-07-19 16:43:46 +0200

    Switched to yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/no-var-requires": "off",
   },
   overrides: [
     {
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const path = require("path");
+
 module.exports = {
   lintOnSave: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: path.resolve("./node_modules/vue/dist/vue.runtime.esm.js"),
+      },
+    },
+  },
 };
