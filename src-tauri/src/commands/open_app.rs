896941d52483da8fd8f07f9e7304b9967e052361
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-22 18:16:15 +0200

    Firefox as default

--- src-tauri/src/commands/open_app.rs
+++ src-tauri/src/commands/open_app.rs
@@ -1,3 +1,7 @@
+use std::env;
+
+use opener::OpenError;
+
 use crate::uis::port_mapping::PortMapping;
 
 #[tauri::command]
@@ -9,15 +13,23 @@ pub fn open_app_ui(app_id: String) -> Result<(), String> {
     .ok_or("App not registered")?;
 
   let app_url = format!("http://localhost:{}", port);
-  tauri::async_runtime::spawn(async move {
-    let result = opener::open(app_url.as_str());
-    log::info!(
-      "Opening app {} at {}, result: {:?}",
-      app_id.clone(),
-      app_url,
-      result
-    );
-  });
+  let result = open_url(app_url.as_str());
+  log::info!(
+    "Opening app {} at {}, result: {:?}",
+    app_id.clone(),
+    app_url,
+    result
+  );
 
+  Ok(())
+}
+
+pub fn open_url(url: &str) -> Result<(), OpenError> {
+  env::set_var("BROWSER", "firefox");
+
+  if let Err(_) = opener::open(url) {
+    env::remove_var("BROWSER");
+    return opener::open(url);
+  }
   Ok(())
 }
