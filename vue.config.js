08b68d6dc05b3056cdcc14ae51cb354aaa326a35
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-09-23 13:55:02 +0200

    Fixed bug

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,22 @@ module.exports = {
         "@material/mwc-dialog": path.resolve(
           "./node_modules/@material/mwc-dialog"
         ),
+        "@material/mwc-snackbar": path.resolve(
+          "./node_modules/@material/mwc-snackbar"
+        ),
+        "@material/mwc-icon-button": path.resolve(
+          "./node_modules/@material/mwc-icon-button"
+        ),
+        "@material/mwc-textarea": path.resolve(
+          "./node_modules/@material/mwc-textarea"
+        ),
+        "@material/mwc-textfield": path.resolve(
+          "./node_modules/@material/mwc-textfield"
+        ),
+        "@material/mwc-fab": path.resolve("./node_modules/@material/mwc-fab"),
+        "@material/mwc-circular-progress": path.resolve(
+          "./node_modules/@material/mwc-circular-progress"
+        ),
       },
     },
   },
