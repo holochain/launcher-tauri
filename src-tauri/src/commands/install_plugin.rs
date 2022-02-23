4d5667bf8d4d15d7db563301d6b0e59ef2df0faf
Kaicho Sun <kaichaosuna@gmail.com>
2022-02-23 18:45:26 +0800

    download file with original format

--- src-tauri/src/commands/install_plugin.rs
+++ src-tauri/src/commands/install_plugin.rs
@@ -4,6 +4,7 @@ use crate::{setup::config::plugins_folder_path, state::LauncherState};
 use std::fs;
 use std::fs::File;
 use std::io::copy;
+use std::io::Cursor;
 use std::os::unix::fs::PermissionsExt;
 
 #[tauri::command]
@@ -18,19 +19,12 @@ pub async fn execute_plugin_install(
     err.to_string()
   })?;
 
-  let plugins_folder = plugins_folder_path();
-
-  let response = reqwest::get(url.clone()).await.map_err(|err| {
-    log::error!("Error install plugin: {}", err);
-    err.to_string()
-  })?;
-
   let fname = url
     .path_segments()
     .and_then(|segments| segments.last())
     .and_then(|name| if name.is_empty() { None } else { Some(name) })
     .ok_or(String::from("Error install plugin: no name provided."))?;
-
+  let plugins_folder = plugins_folder_path();
   log::info!("File to download: '{}'", fname);
   let fname = plugins_folder.join(fname);
   log::info!("File will be located under: '{:?}'", fname);
@@ -38,15 +32,12 @@ pub async fn execute_plugin_install(
     log::error!("Error install plugin: {}", err);
     err.to_string()
   })?;
-  
-  let content = response.text().await.map_err(|err| {
-    log::error!("Error install plugin: {}", err);
-    err.to_string()
-  })?;
-  copy(&mut content.as_bytes(), &mut dest).map_err(|err| {
-    log::error!("Error install plugin: {}", err);
-    err.to_string()
-  })?;
+
+  let response = reqwest::get(url.clone())
+    .await
+    .map_err(|err| err.to_string())?;
+  let mut content = Cursor::new(response.bytes().await.map_err(|err| err.to_string())?);
+  copy(&mut content, &mut dest).map_err(|err| err.to_string())?;
 
   fs::set_permissions(fname, fs::Permissions::from_mode(0o755)).map_err(|err| err.to_string())?;
 
