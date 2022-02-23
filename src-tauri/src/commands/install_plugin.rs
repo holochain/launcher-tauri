168382bc3bfe4ddd27938e0bed17e639217ccf0f
Kaicho Sun <kaichaosuna@gmail.com>
2022-02-23 16:41:03 +0800

    execute plugin binary

--- src-tauri/src/commands/install_plugin.rs
+++ src-tauri/src/commands/install_plugin.rs
@@ -1,8 +1,10 @@
 use reqwest::Url;
 
 use crate::{setup::config::plugins_folder_path, state::LauncherState};
+use std::fs;
 use std::fs::File;
 use std::io::copy;
+use std::os::unix::fs::PermissionsExt;
 
 #[tauri::command]
 pub async fn execute_plugin_install(
@@ -23,21 +25,20 @@ pub async fn execute_plugin_install(
     err.to_string()
   })?;
 
-  let mut dest = {
-    let fname = url
-      .path_segments()
-      .and_then(|segments| segments.last())
-      .and_then(|name| if name.is_empty() { None } else { Some(name) })
-      .ok_or(String::from("Error install plugin: no name provided."))?;
+  let fname = url
+    .path_segments()
+    .and_then(|segments| segments.last())
+    .and_then(|name| if name.is_empty() { None } else { Some(name) })
+    .ok_or(String::from("Error install plugin: no name provided."))?;
 
-    log::info!("File to download: '{}'", fname);
-    let fname = plugins_folder.join(fname);
-    log::info!("File will be located under: '{:?}'", fname);
-    File::create(fname).map_err(|err| {
-      log::error!("Error install plugin: {}", err);
-      err.to_string()
-    })?
-  };
+  log::info!("File to download: '{}'", fname);
+  let fname = plugins_folder.join(fname);
+  log::info!("File will be located under: '{:?}'", fname);
+  let mut dest = File::create(fname.clone()).map_err(|err| {
+    log::error!("Error install plugin: {}", err);
+    err.to_string()
+  })?;
+  
   let content = response.text().await.map_err(|err| {
     log::error!("Error install plugin: {}", err);
     err.to_string()
@@ -46,6 +47,8 @@ pub async fn execute_plugin_install(
     log::error!("Error install plugin: {}", err);
     err.to_string()
   })?;
+
+  fs::set_permissions(fname, fs::Permissions::from_mode(0o755)).map_err(|err| err.to_string())?;
 
   Ok(())
 }
--- src-tauri/src/launch.rs
+++ src-tauri/src/launch.rs
@@ -73,7 +73,9 @@ pub async fn launch_children_processes(running_ports: RunningPorts) -> Result<()
 
   setup_conductor(running_ports.admin_interface_port).await?;
 
-  caddy::launch_caddy(running_ports).await?;
+  caddy::launch_caddy(running_ports.clone()).await?;
+
+  launch_plugins(running_ports).await?;
 
   Ok(())
 }
@@ -97,6 +99,75 @@ async fn setup_conductor(admin_port: u16) -> Result<(), String> {
       .await
       .or(Err(String::from("Could not attach app interface")))?;
     log::info!("Attached app interface to {}", free_port);
+  }
+
+  Ok(())
+}
+
+async fn launch_plugins(running_ports: RunningPorts) -> Result<(), String> {
+  if config::plugins_folder_path().join("ad4m-macos-x64").exists() {
+    // TODO the binary file name needs to load dynamically,
+    // the command needs to specify in config,
+    // init command is not good, but necessary for now.
+    let init_cmd = Command::new(
+      config::plugins_folder_path()
+        .join("ad4m-macos-x64")
+        .to_str()
+        .unwrap(),
+    )
+    .args(&["init"]);
+    let (mut rx, _) = init_cmd.spawn().unwrap();
+
+    tauri::async_runtime::spawn(async move {
+      while let Some(event) = rx.recv().await {
+        match event.clone() {
+          CommandEvent::Stdout(line) => log::info!("[Ad4m] {}", line),
+          CommandEvent::Stderr(line) => log::info!("[Ad4m] {}", line),
+          _ => log::info!("[Ad4m] {:?}", event),
+        }
+      }
+    });
+    log::info!("Initialized ad4m service");
+    thread::sleep(Duration::from_millis(20000));
+
+    let mut ws = AdminWebsocket::connect(format!(
+      "ws://localhost:{}",
+      running_ports.admin_interface_port
+    ))
+    .await
+    .or(Err(String::from("Could not connect to conductor")))?;
+    let app_interfaces = ws
+      .list_app_interfaces()
+      .await
+      .or(Err(String::from("Could not list app interfaces")))?;
+
+    let serve_cmd = Command::new(
+      config::plugins_folder_path()
+        .join("ad4m-macos-x64")
+        .to_str()
+        .unwrap(),
+    )
+    .args(&[
+      "serve",
+      "--connectHolochain",
+      "--hcAdminPort",
+      &running_ports.admin_interface_port.to_string(),
+      "--hcAppPort",
+      &app_interfaces[0].to_string(),
+    ]);
+    let (mut rx, _) = serve_cmd.spawn().unwrap();
+
+    tauri::async_runtime::spawn(async move {
+      while let Some(event) = rx.recv().await {
+        match event.clone() {
+          CommandEvent::Stdout(line) => log::info!("[Ad4m] {}", line),
+          CommandEvent::Stderr(line) => log::info!("[Ad4m] {}", line),
+          _ => log::info!("[Ad4m] {:?}", event),
+        }
+      }
+    });
+    log::info!("Launched ad4m service");
+    thread::sleep(Duration::from_millis(1000));
   }
 
   Ok(())
