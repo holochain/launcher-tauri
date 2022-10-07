14679418ba2f325480f6f814ebdb33bfbb99e864
Matthias <tuttiacc@hotmail.com>
2022-10-07 10:20:34 +0200

    changed version number

--- src/components/settings/About.vue
+++ src/components/settings/About.vue
@@ -23,7 +23,7 @@
         >About</span
       > -->
       <div class="column">
-        <span> Holochain Launcher v0.6.1 </span>
+        <span> Holochain Launcher v0.7.0 </span>
         <span
           style="margin-top: 8px"
           v-for="version in holochainVersions"
