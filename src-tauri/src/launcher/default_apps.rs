63c40966552b9369c0386b0d721ed7f66b379edc
Matthias <tuttiacc@hotmail.com>
2022-11-24 12:17:51 +0100

    changed devhub network seed

--- src-tauri/src/launcher/default_apps.rs
+++ src-tauri/src/launcher/default_apps.rs
@@ -16,7 +16,7 @@ pub async fn install_default_apps_if_necessary(manager: &mut WebAppManager) -> R
     let version: String = manager.holochain_manager.version.into();
 
     // let network_seed = if cfg!(debug_assertions) { Some(String::from("launcher-dev")) } else { None };
-    let network_seed = Some(String::from("DevHub-0.0.172-DEBUG-fw9e8ru39"));
+    let network_seed = Some(String::from("gossipinfo2"));
 
     manager
       .install_web_app(
