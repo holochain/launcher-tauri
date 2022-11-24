7e8c10e66689c92ff9cc266a73140363f27b9179
Matthias <tuttiacc@hotmail.com>
2022-11-24 15:18:39 +0100

    removed line that creates special devhub network seed

--- src-tauri/src/launcher/default_apps.rs
+++ src-tauri/src/launcher/default_apps.rs
@@ -15,8 +15,7 @@ pub async fn install_default_apps_if_necessary(manager: &mut WebAppManager) -> R
     // let version: String = manager.holochain_manager.version.manager().hdi_version().into();
     let version: String = manager.holochain_manager.version.into();
 
-    // let network_seed = if cfg!(debug_assertions) { Some(String::from("launcher-dev")) } else { None };
-    let network_seed = Some(String::from("gossipinfo2"));
+    let network_seed = if cfg!(debug_assertions) { Some(String::from("launcher-dev")) } else { None };
 
     manager
       .install_web_app(
