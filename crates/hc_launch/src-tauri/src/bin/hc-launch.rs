721a7ead648f7084939bbb6a7bd3bcba0e3017d6
Guillem Córdoba <guillem.cordoba@gmail.com>
2022-12-28 17:00:35 +0100

    Panic without env vars

--- crates/hc_launch/src-tauri/src/bin/hc-launch.rs
+++ crates/hc_launch/src-tauri/src/bin/hc-launch.rs
@@ -2,6 +2,12 @@ use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+  if std::env::var_os("WEBKIT_DISABLE_COMPOSITING_MODE").is_none() {
+    panic!("WEBKIT_DISABLE_COMPOSITING_MODE is not set!");
+  }
+  if std::env::var_os("GIO_MODULE_DIR").is_none() {
+    panic!("GIO_MODULE_DIR is not set!");
+  }
   if std::env::var_os("RUST_LOG").is_some() {
     observability::init_fmt(observability::Output::Log).ok();
   }
