82fd7e820b396180397b9603bc672859a5b957b2
Matthias <tuttiacc@hotmail.com>
2023-01-06 13:34:25 +0100

    removed unnecessary line

--- crates/holochain_manager/src/launch.rs
+++ crates/holochain_manager/src/launch.rs
@@ -71,7 +71,6 @@ pub async fn launch_holochain_process(
         // --------------------------------------
         #[cfg(target_family="windows")]
         if line.contains("websocket_error_from_network=Io") && line.contains("ConnectionReset") {
-          fatal_error = true;
           launch_state = LaunchHolochainProcessState::InitializeConductorError(
             InitializeConductorError::AddressAlreadyInUse(
               String::from("Could not initialize Conductor from configuration: \"Address already in use\"")
