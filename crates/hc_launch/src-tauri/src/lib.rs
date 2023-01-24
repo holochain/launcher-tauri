76a3fac5bbf69eb1a3cd4be2b5a32b323be367dd
Matthias <tuttiacc@hotmail.com>
2023-01-24 10:36:54 +0100

    updated hc launch docs

--- crates/hc_launch/src-tauri/src/lib.rs
+++ crates/hc_launch/src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 //! A CLI to launch holochain apps in a Holochain Launcher environment for testing and development purposes.
 //!
-//! If you find a bug please open an [issue](https://github.com/holochain/launcher/issues)
+//! If you find a bug or have a feature request, please open an [issue](https://github.com/holochain/launcher/issues)
 //! or make a PR.
 //!
 //!
@@ -27,6 +27,14 @@
 //! ```
 //! <br>
 //!
+//! * Launch a .happ while pointing to a port on localhost from where you serve the UI assets:<br>
+//! **Note:** This mode is only meant for development purposes! Serving assets from localhost will result in different
+//! behavior than running the app in the real Holochain Launcher.
+//! ```bash
+//! echo pass | hc-launch --piped -n 2 path/to/my/app.happ --ui-port 5500 network mdns
+//! ```
+//! <br>
+//!
 //! * Show the help section with all the available commands and options:
 //! ```bash
 //! hc-launch --help
@@ -34,52 +42,59 @@
 //!
 //! ## Available Commands
 //!
-//! ```
+//! ```text
 //! USAGE:
-//! hc-launch [FLAGS] [OPTIONS] [path] [SUBCOMMAND]
+//!    hc-launch [FLAGS] [OPTIONS] [path] [SUBCOMMAND]
 //!
 //! FLAGS:
-//!     -h, --help
-//!             Prints help information
+//!    -h, --help
+//!            Prints help information
 //!
-//!         --piped
-//!             Instead of the normal "interactive" passphrase mode, collect the passphrase by reading stdin to the end
+//!        --piped
+//!            Instead of the normal "interactive" passphrase mode, collect the passphrase by reading stdin to the end
 //!
-//!     -V, --version
-//!             Prints version information
+//!    -V, --version
+//!            Prints version information
 //!
-//!     -w, --watch
-//!             Watch for file changes in the UI folder. Requires --ui-path to be specified
+//!    -w, --watch
+//!            Watch for file changes in the UI folder. Requires --ui-path to be specified
 //!
 //!
 //! OPTIONS:
-//!     -d, --directories <directories>...
-//!             Specify the directory name for each sandbox that is created. By default, new sandbox directories get a
-//!             random name like "kAOXQlilEtJKlTM_W403b". Use this option to override those names with something explicit.
+//!    -d, --directories <directories>...
+//!            Specify the directory name for each sandbox that is created. By default, new sandbox directories get a
+//!            random name like "kAOXQlilEtJKlTM_W403b". Use this option to override those names with something explicit.
 //!
-//!             For example `hc gen -r path/to/my/chains -n 3 -d=first,second,third` will create three sandboxes with
-//!             directories named "first", "second", and "third".
-//!         --holochain-path <holochain-path>
-//!             Set the path to the holochain binary [env: HC_HOLOCHAIN_PATH=]  [default: holochain]
+//!            For example `hc gen -r path/to/my/chains -n 3 -d=first,second,third` will create three sandboxes with
+//!            directories named "first", "second", and "third".
+//!        --holochain-path <holochain-path>
+//!            Set the path to the holochain binary [env: HC_HOLOCHAIN_PATH=]  [default: holochain]
 //!
-//!     -n, --num-sandboxes <num-sandboxes>
-//!             Number of conductor sandboxes to create [default: 1]
+//!    -n, --num-sandboxes <num-sandboxes>
+//!            Number of conductor sandboxes to create [default: 1]
 //!
-//!         --root <root>
-//!             Set a root directory for conductor sandboxes to be placed into. Defaults to the system's temp directory.
-//!             This directory must already exist
-//!         --ui-path <ui-path>
-//!             path to the UI. Required if a .happ file is passed
+//!        --root <root>
+//!            Set a root directory for conductor sandboxes to be placed into. Defaults to the system's temp directory.
+//!            This directory must already exist
 //!
+//!        --ui-path <ui-path>
+//!            path to the UI. Required if a .happ file is passed
+//!
+//!        --ui-port <ui-port>
+//!            Port pointing to a localhost server that serves your assets. NOTE: This is only meant for development
+//!            purposes! Apps can behave differently when served from a localhost server than when actually running in the
+//!            Holochain Launcher. Use the --ui-path flag pointing to your built and bundled files instead or directly pass
+//!            the packaged .webhapp to test the actual behavior of your hApp in the Holochain Launcher
 //!
 //! ARGS:
-//!     <path>
-//!             Path to .webhapp or .happ file to launch. If a .happ file is passed, a UI path must be specified as well via
-//!             --ui-path
+//!    <path>
+//!            Path to .webhapp or .happ file to launch. If a .happ file is passed, either a UI path must be specified via
+//!            --ui-path or a port pointing to a localhost server via --ui-port
 //!
 //! SUBCOMMANDS:
-//!     help       Prints this message or the help of the given subcommand(s)
-//!     network
+//!    help       Prints this message or the help of the given subcommand(s)
+//!    network
+//!
 //!
 //! ```
 //!
