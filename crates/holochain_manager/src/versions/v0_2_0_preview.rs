53789a0fcc4e00f19a1efacc35fc84d3b5890873
Matthias <tuttiacc@hotmail.com>
2023-03-10 17:58:43 +0100

    added holochain version from develop and changed conductor config to webrtc

--- crates/holochain_manager/src/versions/v0_2_0_preview.rs
+++ crates/holochain_manager/src/versions/v0_2_0_preview.rs
@@ -2,17 +2,18 @@ use std::{path::PathBuf, sync::Arc};
 use url2::Url2;
 
 // NEW_VERSION: Import the exact same types but from the new crates
-use holochain_conductor_api_0_1_3::{
+use holochain_conductor_api_0_2_0_preview::{
   conductor::{ConductorConfig, KeystoreConfig},
   AdminInterfaceConfig, InterfaceDriver,
 };
-use holochain_p2p_0_1_3::kitsune_p2p::{KitsuneP2pConfig, ProxyConfig, TransportConfig, dependencies::kitsune_p2p_types::config::tuning_params_struct::KitsuneP2pTuningParams};
+
+use holochain_p2p_0_2_0_preview::kitsune_p2p::{KitsuneP2pConfig, ProxyConfig, TransportConfig, dependencies::kitsune_p2p_types::config::tuning_params_struct::KitsuneP2pTuningParams};
 
 use super::{version_manager::VersionManager, HdkVersion, HdiVersion, common::{proxy_url, bootstrap_service}};
 
-pub struct HolochainV0_1_3;
+pub struct HolochainV0_2_0_beta_591a74;
 
-impl VersionManager for HolochainV0_1_3 {
+impl VersionManager for HolochainV0_2_0_beta_591a74 {
   // NEW_VERSION: Careful! Indicate here which HDK version comes bundled with this Holochain version
   fn hdk_version(&self) -> HdkVersion {
     HdkVersion::V0_1_1
@@ -37,16 +38,18 @@ impl VersionManager for HolochainV0_1_3 {
 
     network_config.tuning_params = Arc::new(tuning_params);
 
-    network_config.transport_pool.push(TransportConfig::Proxy {
-      sub_transport: Box::new(TransportConfig::Quic {
-        bind_to: None,
-        override_host: None,
-        override_port: None,
-      }),
-      proxy_config: ProxyConfig::RemoteProxyClient {
-        proxy_url: proxy_url(),
-      },
-    });
+    network_config.transport_pool.push(TransportConfig::WebRTC { signal_url: String::from("wss://holotest.net") } );
+
+    // network_config.transport_pool.push(TransportConfig::Proxy {
+    //   sub_transport: Box::new(TransportConfig::Quic {
+    //     bind_to: None,
+    //     override_host: None,
+    //     override_port: None,
+    //   }),
+    //   proxy_config: ProxyConfig::RemoteProxyClient {
+    //     proxy_url: proxy_url(),
+    //   },
+    // });
 
     let config = ConductorConfig {
       environment_path: conductor_environment_path.into(),
