651e19ae674f1b573259ecabf0abffb648956e51
Matthias <tuttiacc@hotmail.com>
2023-03-23 15:43:03 +0100

    removed TalkingStickies default app

--- src-tauri/src/launcher/default_apps.rs
+++ src-tauri/src/launcher/default_apps.rs
@@ -6,10 +6,10 @@ use holochain_web_app_manager::WebAppManager;
 
 
 pub async fn install_default_apps_if_necessary(manager: &mut WebAppManager, window: tauri::window::Window) -> Result<(), String> {
-  let apps = manager.list_apps().await?;
+  // let apps = manager.list_apps().await?;
 
-  // let version: String = manager.holochain_manager.version.manager().hdi_version().into();
-  let holochain_version: String = manager.holochain_manager.version.into();
+  // // let version: String = manager.holochain_manager.version.manager().hdi_version().into();
+  // let holochain_version: String = manager.holochain_manager.version.into();
 
   // let devhub_app_id = format!("DevHub-{}", holochain_version);
 
@@ -39,31 +39,6 @@ pub async fn install_default_apps_if_necessary(manager: &mut WebAppManager, wind
   //     )
   //     .await?;
   // }
-
-
-  // ====================== install Talking Stickies for testing purposes =================================
-  // emitting signal to the front-end for progress indication
-  window.emit("progress-update", String::from("Installing TalkingStickies"))
-    .map_err(|e| format!("Failed to send signal to the frontend: {:?}", e))?;
-
-  let ts_bundle = WebAppBundle::decode(include_bytes!("../../../TalkingStickies.webhapp"))
-    .or(Err("Malformed Web hApp bundle file"))?;
-
-  let ts_network_seed = if cfg!(debug_assertions) { Some(String::from("testing-webrtc-0.2.0-beta-rc.1-dev")) } else { Some(String::from("testing-webrtc-0.2.0-beta-rc.1")) };
-
-  manager
-    .install_web_app(
-      String::from("TalkingStickies WebRTC Test"),
-      ts_bundle,
-      ts_network_seed,
-      HashMap::new(),
-      None,
-      None,
-      None,
-    )
-    .await?;
-
-  // ===============================================================================
 
   Ok(())
 }
