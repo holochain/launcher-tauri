37e69d3c55f58122e23dfab938f4dc4123801bf7
Tibet Sprague <tibet.sprague@gmail.com>
2023-05-14 16:20:04 -0700

    Fix Vue warnings

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: tag => tag.startsWith('sl-') || tag.startsWith('mwc-')
+      }
+    }
+  })],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
