30a9b76eca9c110500333de3ed044f1a05f13e63
Matthias <tuttiacc@hotmail.com>
2023-09-07 09:16:05 +0200

    fixed use of wrong app icon when installing app from file system

--- src/views/AppStore.vue
+++ src/views/AppStore.vue
@@ -508,6 +508,7 @@ export default defineComponent({
       }
     },
     async selectFromFileSystem() {
+      this.selectedIconSrc = undefined;
       this.selectedAppBundlePath = (await open({
         filters: [
           { name: "Holochain Application", extensions: ["webhapp", "happ"] },
