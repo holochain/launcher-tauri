7e71591900af990573f5d33b501fbdcd02e38d32
Matthias <tuttiacc@hotmail.com>
2023-09-18 13:10:29 +0200

    use no cache when fetchin gfilterlist

--- src/views/AppStore.vue
+++ src/views/AppStore.vue
@@ -317,7 +317,8 @@ export default defineComponent({
     try {
       // Temporary solution: Fetch centrally stored filter list to filter out orphaned apps in the appstore.
       const response = await fetch(
-        "https://s3.eu-central-2.wasabisys.com/holochain-launcher/filter-lists/appstore_filter_list_hc0.1.json"
+        "https://s3.eu-central-2.wasabisys.com/holochain-launcher/filter-lists/appstore_filter_list_hc0.1.json",
+        { cache: "no-cache" }
       );
       const filterList: Array<FilterListEntry> = await response.json();
       this.filterList = filterList;
