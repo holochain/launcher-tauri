b96d5aa790bf0da0a8f0c44741276f7f9c4b6b41
Matthias <tuttiacc@hotmail.com>
2024-04-29 15:22:50 +0200

    fixed version number

--- crates/hc_launch/src-tauri/src/cli.rs
+++ crates/hc_launch/src-tauri/src/cli.rs
@@ -22,7 +22,7 @@ use crate::prepare_webapp;
 use holochain_cli_sandbox::cmds::{Create, Existing, NetworkCmd, NetworkType};
 
 #[derive(Debug, Parser)]
-#[command(version = "0.300.0-dev.0 (holochain 0.3.0-beta-dev.44)")]
+#[command(version = "0.300.0-dev.1 (holochain 0.3.0-beta-dev.48)")]
 #[command(author, about, long_about = None)]
 /// Helper for launching holochain apps in a Holochain Launcher environment for testing and development purposes.
 ///
